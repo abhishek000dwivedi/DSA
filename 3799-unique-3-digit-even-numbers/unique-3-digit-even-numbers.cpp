6308d748dfcf4fb6f92cce091f90dda68f3415fb
abhishek000dwivedi <109316578+abhishek000dwivedi@users.noreply.github.com>
2026-09-12 12:15:26 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (88.72%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-       int n = digits.size();
-       set<int> st;
-       int count=0;
-       for(int i =0; i<n;i++){
-        for(int j=0; j<n; j++){
-            for(int k=0; k<n; k++){
-                if(i==j || i==k || j==k) continue;
-               else if(digits[i]==0) continue;
-                else if(digits[k]%2==0){
-                     count++;
-                     int num= digits[i]*100+ digits[j]*10+ digits[k];
-                     st.insert(num);
-                     }
-                 
+       int f[10]={0};
+       int ans=0;
+       int n =digits.size();
+
+       for(int i =0; i<n ; i++){
+        f[digits[i]]++;
+       }
+
+       for(int i =1; i<10; i++){
+        for(int j=0; j<10 ;j++){
+            for(int k=0; k<9; k+=2){
+             ans= ans+ (f[i]>0 && f[j]>(i==j) && f[k]>(i==k)+(j==k));
+
             }
         }
        }
-       return st.size();
 
-    }
+return ans;
+         }
 };
