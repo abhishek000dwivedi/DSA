2708da379b8796ca4c4ce6afb1cc9db863ad6c8e
abhishek000dwivedi <109316578+abhishek000dwivedi@users.noreply.github.com>
2026-09-13 16:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (75.46%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-        vector<int> ans;
-        int count=0;
+        vector<int> ans(nums1.size());
+        stack<int> st;
+        unordered_map<int,int> mpp;
 
-        for(int i=0; i<nums1.size(); i++){
-            
-            bool flag= false;
-            count=0;
-            for(int j=0; j<nums2.size(); j++){
+        for(int i= nums2.size()-1; i>=0 ;i--){
 
-                if(nums1[i]==nums2[j]) flag= true;
-
-                if(flag){
-                    if(nums2[j]>nums1[i]) 
-                    {
-                        ans.push_back(nums2[j]) ;
-                        count++;
-                        break;
-                        }
-                }
-              
+            while(!st.empty() && st.top()<= nums2[i]){
+                st.pop();
             }
-            if(count==0) ans.push_back(-1);
+
+            if(st.empty()){
+                mpp[nums2[i]]=-1;
+
+            }
+            else{
+               mpp[nums2[i]]= st.top();
+
+            }
+            st.push(nums2[i]);
             
-            
+
+        }
+
+        for(int i =0; i<nums1.size();i++){
+            ans[i]= mpp[nums1[i]];
         }
 
         return ans;
