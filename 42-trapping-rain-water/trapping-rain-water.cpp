eeae980ebe4f2533a6cc7069c23edeec844e620c
abhishek000dwivedi <109316578+abhishek000dwivedi@users.noreply.github.com>
2026-09-16 18:06:37 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (6.05%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -10,21 +10,23 @@ public:
             maxPrefix[0]= height[0];
             maxSuffix[n-1]=height[n-1];
 
-            for(int i =1; i<n; i++){
-                maxPrefix[i]= max(height[i],maxPrefix[i-1]);
+            // for(int i =1; i<n; i++){
+            //     maxPrefix[i]= max(height[i],maxPrefix[i-1]);
                 
-            }
+            // }
 
             for(int i=n-2; i>=0; i--){
                 maxSuffix[i]= max(height[i],maxSuffix[i+1]);
             }
 
+            int leftmax=0;
+
 
         for(int i=0; i< height.size(); i++){
 
-           
+            leftmax= max(height[i],leftmax);
             //  if( min(maxPrefix[i],maxSuffix[i])-height[i] >0 )    
-            total = total + min(maxPrefix[i],maxSuffix[i])-height[i];
+            total = total + min(leftmax,maxSuffix[i])-height[i];
 
         }
 
