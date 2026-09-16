9b9c2f5279ba1a7b62b4767710031ac24ba73732
abhishek000dwivedi <109316578+abhishek000dwivedi@users.noreply.github.com>
2026-09-16 17:24:35 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (16.36%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+            int total=0;
+            int n = height.size();
+            vector<int> maxPrefix(n,0);
+            vector<int> maxSuffix(n,0);
+
+            maxPrefix[0]= height[0];
+            maxSuffix[n-1]=height[n-1];
+
+            for(int i =1; i<n; i++){
+                maxPrefix[i]= max(height[i],maxPrefix[i-1]);
+                
+            }
+
+            for(int i=n-2; i>=0; i--){
+                maxSuffix[i]= max(height[i],maxSuffix[i+1]);
+            }
+
+
+        for(int i=0; i< height.size(); i++){
+
+           
+            //  if( min(maxPrefix[i],maxSuffix[i])-height[i] >0 )    
+            total = total + min(maxPrefix[i],maxSuffix[i])-height[i];
+
+        }
+
+    return  total;
+         
+    }
+};
